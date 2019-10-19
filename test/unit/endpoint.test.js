905cd43cba2fc9bcc9d9a4b801b7e60a4548b405
Benjamin Leov <benleov@gmail.com>
2019-10-19 18:12:21 +1100

    Test status page returns 200 response

--- test/unit/endpoint.test.js
+++ test/unit/endpoint.test.js
@@ -3,12 +3,20 @@ const request = require('supertest')
 const server = require('../../src/index')
 
 describe('root endpoint', () => {
+
     it('should return hello world', async () => {
         const res = await request(server)
             .get('/')
             .send()
         expect(res.statusCode).toEqual(200)
         expect(res.text).toEqual("Hello World")
+    })
+
+    it('should return status', async () => {
+        const res = await request(server)
+            .get('/status')
+            .send()
+        expect(res.statusCode).toEqual(200)
     })
 
     afterAll(done => {
