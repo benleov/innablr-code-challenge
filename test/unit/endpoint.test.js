cd4b6b6ef50bf4272adb2b69b9c5b30012a81877
Benjamin Leov <benleov@gmail.com>
2019-10-19 18:00:49 +1100

    Added test for root endpoint

---
+++ test/unit/endpoint.test.js
@@ -0,0 +1,18 @@
+// application tests go here
+const request = require('supertest')
+const server = require('../../src/index')
+
+describe('root endpoint', () => {
+    it('should return hello world', async () => {
+        const res = await request(server)
+            .get('/')
+            .send()
+        expect(res.statusCode).toEqual(200)
+        console.log(res)
+         expect(res.text).toEqual("Hello World")
+    })
+
+    afterAll(done => {
+        server.close(done)
+    })
+})
